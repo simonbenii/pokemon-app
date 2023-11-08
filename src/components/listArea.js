396bcede256d395a9a5ec716ad5c1d98f15183b9
“simonbenii” <110835614+simonbenii@users.noreply.github.com>
2023-11-08 14:10:21 +0100

    change listAreas links

--- src/components/listArea.js
+++ src/components/listArea.js
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
 import "../ListArea.css";
 
-import canalaveCity from "../img/HGSS_Seafoam_Islands-Day.png";
-import eternaCity from "../img/HGSS_Viridian_Forest-Day.png";
-import pastoriaCity from "../img/HGSS_Victory_Road-Day.png";
-import sunyshoreCity from "../img/HGSS_Ruins_of_Alph-Day.png";
-import sinnohPokemonLeague from "../img/HGSS_National_Park-Day.png";
-import oreburghMine from "../img/HGSS_Mt._Mortar-Day.png";
-import valleyWindworks from "../img/HGSS_Cerulean_Cave-Night.png";
-import eternaForest from "../img/HGSS_Ilex_Forest-Day.png";
-import fuegoIronworks from "../img/HGSS_Bell_Tower-Day.png";
-import mtCoronet from "../img/HGSS_Mt._Silver-Day.png";
-import greatMarsh from "../img/HGSS_Slowpoke_Well-Day.png";
-import solaceonRuins from "../img/HGSS_Ruins_of_Alph-Day.png";
-import sinnohVictoryRoad from "../img/HGSS_Victory_Road-Day.png";
-import ravagedPath from "../img/HGSS_Dark_Cave-Route_31-Day.png";
-import orehurghGate from "../img/HGSS_Union_Cave-Day.png";
-import starkMountain from "../img/HGSS_Mt._Silver-Day.png";
-import springPath from "../img/HGSS_Sprout_Tower-Day.png";
-import turnbackCave from "../img/HGSS_Rock_Tunnel-Day.png";
-import snowpointTemple from "../img/HGSS_Ice_Cave-Day.png";
-import waywardCave from "../img/HGSS_Dark_Cave-Route_45-Day.png";
+import canalaveCity from "./../img/HGSS_Seafoam_Islands-Day.png";
+import eternaCity from "./../img/HGSS_Viridian_Forest-Day.png";
+import pastoriaCity from "./../img/HGSS_Victory_Road-Day.png";
+import sunyshoreCity from "./../img/HGSS_Ruins_of_Alph-Day.png";
+import sinnohPokemonLeague from "./../img/HGSS_National_Park-Day.png";
+import oreburghMine from "./../img/HGSS_Mt._Mortar-Day.png";
+import valleyWindworks from "./../img/HGSS_Cerulean_Cave-Night.png";
+import eternaForest from "./../img/HGSS_Ilex_Forest-Day.png";
+import fuegoIronworks from "./../img/HGSS_Bell_Tower-Day.png";
+import mtCoronet from "./../img/HGSS_Mt._Silver-Day.png";
+import greatMarsh from "./../img/HGSS_Slowpoke_Well-Day.png";
+import solaceonRuins from "./../img/HGSS_Ruins_of_Alph-Day.png";
+import sinnohVictoryRoad from "./../img/HGSS_Victory_Road-Day.png";
+import ravagedPath from "./../img/HGSS_Dark_Cave-Route_31-Day.png";
+import orehurghGate from "./../img/HGSS_Union_Cave-Day.png";
+import starkMountain from "./../img/HGSS_Mt._Silver-Day.png";
+import springPath from "./../img/HGSS_Sprout_Tower-Day.png";
+import turnbackCave from "./../img/HGSS_Rock_Tunnel-Day.png";
+import snowpointTemple from "./../img/HGSS_Ice_Cave-Day.png";
+import waywardCave from "./../img/HGSS_Dark_Cave-Route_45-Day.png";
 
 const locationImages = {
   "canalave-city": canalaveCity,
